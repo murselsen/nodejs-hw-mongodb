73793988e42fff13d66d068331f9afd509e8037c
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-28 23:22:02 +0300

    Refactor server.js to streamline PORT declaration and improve readability

--- src/server.js
+++ src/server.js
@@ -1,17 +1,11 @@
 import express from 'express';
 
 export const setupServer = () => {
-
-
   const app = express();
 
+  const PORT = process.env.PORT || 3000;
 
-
-
-
-const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT)
+  app.listen(PORT);
 
   console.log('Server setup complete');
 };
