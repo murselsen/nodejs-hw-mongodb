3a1b42f64b88f408208779238af4900fbadb3889
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-28 23:20:38 +0300

    Add server setup, environment configuration, and dependencies

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
+import express from 'express';
+
 export const setupServer = () => {
+
+
+  const app = express();
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT)
+
   console.log('Server setup complete');
 };
