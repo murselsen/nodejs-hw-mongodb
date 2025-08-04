988f6d2a16a3fcfa332747ead901fdf717f5c38a
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-04 11:42:45 +0300

    feat: add getAllContactsController for fetching contacts

---
+++ src/controllers/contacts.js
@@ -0,0 +1,36 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getAllContactsController = async (req, res) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+    res.status(500).json({ status: 500, message: 'Internal Server Error' });
+  }
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await getContactById(contactId);
+    console.log('Contact found:', contact);
+    if (!contact) {
+      res.status(404).json({
+        status: 404,
+        message: 'Contact not found',
+      });
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    console.error('Error fetching contact by ID:', error);
+  }
+};
