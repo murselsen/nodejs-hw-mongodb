070cf99be4df9ed0acb915320a80d7700db8db86
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-23 04:14:22 +0300

    feat: add main router to handle authentication and contact routes

---
+++ src/routers/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const appRouters = express.Router();
+
+import contactRouter from './contact.js';
+import authRouter from './auth.js';
+
+appRouters.use('/auth', authRouter);
+appRouters.use('/contacts', contactRouter);
+
+export default appRouters;
