3ec2cf913ae900c58046dd0b641928d9c71465d9
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-23 04:14:01 +0300

    feat: implement user registration and login controllers with session token handling

---
+++ src/controllers/auth.js
@@ -0,0 +1,50 @@
+import createHttpError from 'http-errors';
+import { registerUser, loginUser } from '../services/auth.js';
+
+export const registerUserController = async (req, res, next) => {
+  try {
+    const newUser = await registerUser(req.body);
+
+    if (!newUser) next(createHttpError(400, 'User registration failed'));
+    res.status(201).json({
+      status: 'success',
+      message: 'Successfully registered a user!',
+      data: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createSessionTokenCookie = (res, { sessionId, refreshToken }) => {
+    const day30 = 30 * 24 * 60 * 60 * 1000; // 30 days in milliseconds
+  res.cookie('sessionId', sessionId, {
+    httpOnly: true,
+    expires: new Date(Date.now() + day30), // 30 days
+  });
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + day30), // 30 days
+  });
+};
+
+export const loginUserController = async (req, res, next) => {
+  try {
+    const session = await loginUser(req.body);
+    if (!session) next(createHttpError(400, 'Login failed'));
+    createSessionTokenCookie(res, {
+      sessionId: session._id,
+      refreshToken: session.refreshToken,
+    });
+
+    console.log(session);
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Login successful',
+      data: session.accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
