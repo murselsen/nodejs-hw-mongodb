1317eece87db6926a99bd472e45d4b9a5c5e56b5
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-08-23 04:14:31 +0300

    feat: add validation schemas for user registration and login using Joi

---
+++ src/validation/auth.js
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+
+export const registerUserSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    'string.email': 'Email must be a valid email',
+    'any.required': 'Email is required',
+  }),
+  password: Joi.string().required(),
+});
