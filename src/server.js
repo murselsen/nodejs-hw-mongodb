9b59042958d9dbe1f3833d19ebbe8ea9447d5918
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-09-16 13:41:35 +0300

    feat: add Swagger and ReDoc documentation for API endpoints

--- src/server.js
+++ src/server.js
@@ -4,10 +4,14 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
-
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+import path from 'path';
+// Routers
 import appRouters from './routers/index.js';
 
 const PORT = env('PORT') || 3000;
+const swaggerDocument = yaml.load(path.join(process.cwd(), 'swagger.yaml'));
 
 export const setupServer = async () => {
   const app = express();
@@ -15,6 +19,8 @@ export const setupServer = async () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the Contacts API' });
   });
